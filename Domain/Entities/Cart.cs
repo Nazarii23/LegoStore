a2b09a3b8ef5c41ba6240d0a929fabd01c6a81e8
Nazarii23 <nazarkurash@gmail.com>
2021-06-30 19:55:47 +0200

    dodanie przycisku

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        private List<Cartline> LineCollection = new List<Cartline>();
+
+        public IEnumerable<Cartline> lines { get { return LineCollection; } }
+        public void AddItem(Product product, int quantity)
+        {
+            Cartline line = LineCollection
+                .Where(p => p.product.ProductId == product.ProductId)
+                .FirstOrDefault();
+            if(line == null)
+            {
+                LineCollection.Add(new Cartline { product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            LineCollection.RemoveAll(l => l.product.ProductId == product.ProductId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return LineCollection.Sum(e => e.product.Price * e.Quantity);
+        }
+
+        public void Clear()
+        {
+            LineCollection.Clear();
+        }
+    }
+
+    public class Cartline
+    {
+        public Product product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ LegoStore/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LegoStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository productRepository;
+        public CartController(IProductRepository repo)
+        {
+            productRepository = repo;
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if(cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = productRepository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if(product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = productRepository.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+    }
+}
